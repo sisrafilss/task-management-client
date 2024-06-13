a8aa40d19bb4d67d9dea10c4006742741b2662b7
IsrafilHossen3 <israfilhossen3@gmail.com>
2024-06-13 06:11:32 +0600

    navbar and footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
-
-
-const Footer = () => {
-    return (
-        <div>
-            This is Footer!           
-        </div>
-    );
-};
+const Footer = () => (
+  <footer className="bg-blue-500 text-white p-4 mt-4">
+    <p>&copy; 2024 Task Manager. All rights reserved.</p>
+    <ul className="flex justify-center">
+      <li className="mx-2">
+        <a href="https://facebook.com" className="hover:text-gray-300">
+          Facebook
+        </a>
+      </li>
+      <li className="mx-2">
+        <a href="https://twitter.com" className="hover:text-gray-300">
+          Twitter
+        </a>
+      </li>
+      <li className="mx-2">
+        <a href="https://instagram.com" className="hover:text-gray-300">
+          Instagram
+        </a>
+      </li>
+    </ul>
+  </footer>
+);
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,13 @@
-const Navbar = () => {
-  return <div>This is Navbar!</div>;
-};
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="bg-blue-500 p-4">
+    <ul className="flex justify-between text-white">
+      <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
+      <li><Link to="/login" className="hover:text-gray-300">Login</Link></li>
+      <li><Link to="/register" className="hover:text-gray-300">Register</Link></li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
