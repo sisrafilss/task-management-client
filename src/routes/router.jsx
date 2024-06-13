4a3c1bd4f475f6ede2dc69c937f7724775a866a2
IsrafilHossen3 <israfilhossen3@gmail.com>
2024-06-13 06:05:15 +0600

    router setup completed

---
+++ src/routes/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import MainLayout from "../layouts/MainLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
